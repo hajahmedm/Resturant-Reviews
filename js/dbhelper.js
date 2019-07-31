c865fc9f12bd48cbaa49e35125779b41cb9fbfde
Mohammed Hajahmed <47723832+hajahmedm@users.noreply.github.com>
2019-07-30 22:42:17 -0400

    Update dbhelper.js
    fixing bugs in the dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -149,7 +149,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`https://hajahmedm.github.io/Resturant-Reviews/img/${restaurant.photograph}`);
   }
 
   /**
